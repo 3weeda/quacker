eb082fdf667b1c27706933cc718c36ea86e848f0
Ahmad-3weeda <3weeda@gmail.com>
2019-10-22 17:21:35 +0200

    implement home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+import Quack from '../components/Quack';
+import Navbar from '../components/Navbar';
 
 export default class home extends Component {
+    state = {
+        quacks: null
+    }
+    componentDidMount() {
+        axios.get('/screams')
+            .then(res => {
+                console.log(res.data)
+                this.setState({ quacks: res.data })
+            })
+            .catch(err => console.log(err))
+    }
     render() {
+        let recentQuacks = this.state.quacks ? (
+            this.state.quacks.map(quack => <Quack quack={quack} key={quack.screamId} />)
+        ) : <p>Loading...</p>
         return (
-            <div>
-                home page
+            <div className="home-container">
+                <Navbar />
+                <Grid container spacing={2}>
+                    <Grid item sm={8} xs={12}>
+                        {recentQuacks}
+                    </Grid>
+                    <Grid item sm={4} xs={12}>
+                        <p>profile</p>
+                    </Grid>
+                </Grid>
             </div>
         )
     }
