f40adaf661ff32b1244076ddb15356e6e69771dd
Ahmad-3weeda <3weeda@gmail.com>
2019-10-23 23:05:42 +0200

    implement edit user details dialog

---
+++ src/components/EditDetails.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { editUserDetails } from '../redux/actions/userActions';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Tooltip from '@material-ui/core/Tooltip';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+
+const styles = theme => ({
+    ...theme.spreadableStyles,
+    button: {
+        float: 'right'
+    }
+})
+
+class EditDetails extends Component {
+    state = {
+        bio: '',
+        website: '',
+        location: '',
+        open: false
+    }
+
+    componentDidMount() {
+        this.initializeUserDetails(this.props.credentials);
+    }
+
+    handleOpen = () => {
+        this.setState({ open: true })
+        this.initializeUserDetails(this.props.credentials);
+    }
+    handleClose = () => {
+        this.setState({ open: false })
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleSubmit = () => {
+        const userDetails = {
+            bio: this.state.bio,
+            website: this.state.website,
+            location: this.state.location,
+        }
+        this.props.editUserDetails(userDetails);
+        this.handleClose();
+    }
+
+    initializeUserDetails = (credentials) => {
+        this.setState({
+            bio: credentials.bio ? credentials.bio : '',
+            website: credentials.website ? credentials.website : '',
+            location: credentials.location ? credentials.location : ''
+        })
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <>
+                <Tooltip title="Edit details" placement="top">
+                    <IconButton onClick={this.handleOpen} className={classes.button}>
+                        <EditIcon color="primary" />
+                    </IconButton>
+                </Tooltip>
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    fullWidth
+                    maxWidth="sm">
+                    <DialogTitle>Edit your details</DialogTitle>
+                    <DialogContent>
+                        <form>
+                            <TextField
+                                name="bio"
+                                label="Bio"
+                                type="text"
+                                multiline
+                                rows="3"
+                                placeholder="A short bio about yourself"
+                                className={classes.textField}
+                                value={this.state.bio}
+                                onChange={this.handleChange}
+                                fullWidth
+                            />
+                            <TextField
+                                name="website"
+                                label="Website"
+                                type="text"
+                                placeholder="Your personal/professional website"
+                                className={classes.textField}
+                                value={this.state.website}
+                                onChange={this.handleChange}
+                                fullWidth
+                            />
+                            <TextField
+                                name="location"
+                                label="Location"
+                                type="text"
+                                placeholder="Your location"
+                                className={classes.textField}
+                                value={this.state.location}
+                                onChange={this.handleChange}
+                                fullWidth
+                            />
+                        </form>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">Cancel</Button>
+                        <Button onClick={this.handleSubmit} color="primary">Save</Button>
+                    </DialogActions>
+                </Dialog>
+            </>
+        )
+    }
+}
+
+EditDetails.propTypes = {
+    classes: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired,
+    editUserDetails: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    credentials: state.user.credentials
+})
+
+const mapActionsToProps = {
+    editUserDetails
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(EditDetails))
