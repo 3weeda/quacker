cbc1974541912c7b12c0021c7cdf7d71ce2d540b
Ahmad-3weeda <3weeda@gmail.com>
2019-10-23 23:05:59 +0200

    add profile and edit user details dialog to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 import Quack from '../components/Quack';
 import Navbar from '../components/Navbar';
+import Profile from '../components/Profile';
 
 export default class home extends Component {
     state = {
@@ -28,7 +29,7 @@ export default class home extends Component {
                         {recentQuacks}
                     </Grid>
                     <Grid item sm={4} xs={12}>
-                        <p>profile</p>
+                        <Profile />
                     </Grid>
                 </Grid>
             </div>
