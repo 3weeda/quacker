cb1227da30446f05c8d7e076b403ab6e40df9bd3
Ahmad-3weeda <3weeda@gmail.com>
2019-10-23 23:04:35 +0200

    add uploadImage and edit user details actions to redux store

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,4 +1,4 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED } from '../types';
+import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED, LOADING_USER } from '../types';
 import axios from 'axios';
 
 export const loginUser = (userData, history) => dispatch => {
@@ -39,14 +39,14 @@ export const signupUser = (newUserData, history) => dispatch => {
         })
 }
 
-export const logoutUser = history => dispatch => {
+export const logoutUser = () => dispatch => {
     localStorage.removeItem('FBIdToken');
     delete axios.defaults.headers.common['Authorization'];
     dispatch({ type: SET_UNAUTHENTICATED });
-    history.push('/login');
 }
 
 export const getUserData = () => dispatch => {
+    dispatch({ type: LOADING_USER });
     axios.get('/user')
         .then(res => {
             dispatch({
@@ -55,6 +55,24 @@ export const getUserData = () => dispatch => {
             })
         })
         .catch(err => console.log(err));
+}
+
+export const uploadImage = formData => dispatch => {
+    dispatch({ type: LOADING_USER });
+    axios.post('/user/image', formData)
+        .then(() => {
+            dispatch(getUserData());
+        })
+        .catch(err => console.log(err))
+}
+
+export const editUserDetails = userDetails => dispatch => {
+    dispatch({ type: LOADING_USER });
+    axios.post('/user', userDetails)
+        .then(() => {
+            dispatch(getUserData());
+        })
+        .catch(err => console.log(err))
 }
 
 const setAuthorizationHeader = token => {
