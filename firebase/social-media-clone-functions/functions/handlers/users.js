cd0e765dfd0e133ec5a7291f1b347266b1520250
Ahmad-3weeda <3weeda@gmail.com>
2019-10-23 22:56:51 +0200

    update firebase serverless backend (add toString for image names)

--- firebase/social-media-clone-functions/functions/handlers/users.js
+++ firebase/social-media-clone-functions/functions/handlers/users.js
@@ -181,9 +181,8 @@ exports.uploadImage = (req, res) => {
             return res.status(400).json({ error: 'Wrong file type submitted' })
         }
         console.log(fieldname, filename, mimetype)
-        // array fo image name split by dots > access last element to get extension
         const imageExtension = filename.split('.')[filename.split('.').length - 1];
-        imageFileName = `${Math.round(Math.random() * 100000000000000)}.${imageExtension}`;
+        imageFileName = `${Math.round(Math.random() * 1000000000000).toString()}.${imageExtension}`;
         const filePath = path.join(os.tmpdir(), imageFileName);
         imageToBeUploaded = { filePath, mimetype };
         file.pipe(fs.createWriteStream(filePath));
@@ -198,20 +197,19 @@ exports.uploadImage = (req, res) => {
             }
         })
             .then(() => {
-                // alt=media allows the image to be displayed inside the browser
                 const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`
                 return db.doc(`/users/${req.user.handle}`).update({ imageUrl })
             })
             .then(() => {
                 return res.json({ message: 'Image uploaded successfully' });
             })
-            .catch(err => {
-                console.log(err);
-                return res.status(500).json({ error: err.code });
-            })
-    })
+            .catch((err) => {
+                console.error(err);
+                return res.status(500).json({ error: 'something went wrong' });
+            });
+    });
     busboy.end(req.rawBody);
-}
+};
 
 // Read notifications
 exports.markNotificationsRead = (req, res) => {
