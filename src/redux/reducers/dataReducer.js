3c44bf8000e5929ed8f25a8043a85cce3e07f485
Ahmad-3weeda <3weeda@gmail.com>
2019-10-26 20:28:39 +0200

    add dataReducer full logic

---
+++ src/redux/reducers/dataReducer.js
@@ -0,0 +1,61 @@
+import { SET_QUACKS, SET_QUACK, LOADING_DATA, LIKE_QUACK, UNLIKE_QUACK, DELETE_QUACK, POST_QUACK } from '../types';
+
+const initialState = {
+    quacks: [],
+    quack: {},
+    loading: false
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case LOADING_DATA:
+            return {
+                ...state,
+                loading: true
+            }
+        case SET_QUACKS:
+            return {
+                ...state,
+                quacks: action.payload,
+                loading: false
+            }
+        case SET_QUACK:
+            return {
+                ...state,
+                quack: action.payload,
+                loading: false
+            }
+        case LIKE_QUACK:
+        case UNLIKE_QUACK:
+            let index = state.quacks.findIndex(quack => quack.screamId === action.payload.screamId)
+            //replace liked quack with another one
+            state.quacks[index] = action.payload
+            // update number of likes
+            if (state.quack.screamId === action.payload.screamId) {
+                state.quack = action.payload
+            }
+            return {
+                ...state,
+            }
+        case DELETE_QUACK:
+            // index = state.quacks.findIndex(quack => quack.screamId === action.payload.screamId);
+            // state.quacks.splice(index, 1);
+            // return {
+            //     ...state,
+            // }
+            return {
+                ...state,
+                quacks: state.quacks.filter(quack => quack.screamId !== action.payload)
+            }
+        case POST_QUACK:
+            return {
+                ...state,
+                quacks: [
+                    action.payload,
+                    ...state.quacks
+                ]
+            }
+        default:
+            return state;
+    }
+}
