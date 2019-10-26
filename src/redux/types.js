1b00245bb251c9f7c8758b59444c441dc9d7e6e6
Ahmad-3weeda <3weeda@gmail.com>
2019-10-26 20:28:53 +0200

    update redux store action types

--- src/redux/types.js
+++ src/redux/types.js
@@ -7,4 +7,12 @@ export const LOADING_USER = 'LOADING_USER';
 export const SET_ERRORS = 'SET_ERRORS';
 export const LOADING_UI = 'LOADING_UI';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const STOP_LOADING_UI = "STOP_LOADING_UI";
 // Data reducer types
+export const SET_QUACKS = "SET_QUACKS";
+export const SET_QUACK = "SET_QUACK";
+export const LOADING_DATA = "LOADING_DATA";
+export const POST_QUACK = "POST_QUACK";
+export const LIKE_QUACK = "LIKE_QUACK";
+export const UNLIKE_QUACK = "UNLIKE_QUACK";
+export const DELETE_QUACK = "DELETE_QUACK";
